1f276258f1a0ad8548590c666c32688615b8466d
refdinal22 <refdinal22@gmail.com>
2020-05-18 13:56:50 +0700

    add unit test for get width, height, and area of object

--- retrieval/test_detector.py
+++ retrieval/test_detector.py
@@ -4,6 +4,7 @@ import skimage.io
 from detector import Detector
 
 detector = Detector("../weight/mask_rcnn_fashion.h5")
+image_input = [10,10,20,20]
 
 class TestDetector(unittest.TestCase):
 	def test_detection(self):
@@ -23,3 +24,15 @@ class TestDetector(unittest.TestCase):
 
 	def test_wrong_input(self):		
 		self.assertRaises(ValueError, detector.detection, "Wrong input")    
+
+	def test_get_width_of_object(self):		
+		width = detector.get_width(image_input)
+		self.assertEqual(width, 10)
+
+	def test_get_height_of_object(self):		
+		height = detector.get_height(image_input)
+		self.assertEqual(height, 10)
+
+	def test_get_area_of_object(self):		
+		area = detector.get_area(image_input)
+		self.assertEqual(area, 100)
