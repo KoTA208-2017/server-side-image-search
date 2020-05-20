42564409cf00e03e76bc32ad930e9bf143bdcc11
refdinal22 <refdinal22@gmail.com>
2020-05-20 14:04:32 +0700

    feat(feature-exraction): add connection to database using sqlalchemy

---
+++ server/database/database.py
@@ -0,0 +1,30 @@
+# 1 - imports
+import json
+from sqlalchemy import func
+from . import base
+from .model import Product
+from sqlalchemy.ext.declarative import DeclarativeMeta
+
+class DAO:
+	def __init__(self):
+		self.session = base.Session()
+
+	def getAll(self):
+		products = self.session.query(Product).all()		
+		return products		
+
+	def getProduct(self, ids):
+		q = self.session.query(Product)
+		products = q.filter(Product.id.in_(ids)).order_by(func.field(Product.id, *ids))
+		return products
+
+	def insert(self, name, price, image, url, ecommerce):
+		product = Product(name, price, image, url, ecommerce)
+		self.session.add(product)
+
+		self.session.commit()
+		self.session.close()
+	
+
+        
+
