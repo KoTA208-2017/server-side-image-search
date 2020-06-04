2a244795790e185fc8388a5662ff607778824e13
refdinal22 <refdinal22@gmail.com>
2020-06-04 14:18:31 +0700

    feat(restAPI-server): adjust product classes with tables in the database

--- server/database/model.py
+++ server/database/model.py
@@ -4,18 +4,22 @@ from . import base
 from sqlalchemy_serializer import SerializerMixin
 
 class Product(base.Base, SerializerMixin):
-	__tablename__ = 'product'
+	__tablename__ = 'tbm_product'
 
 	id = Column(Integer, primary_key=True)
+	siteName = Column(String)
 	name = Column(String)
 	price = Column(Integer)
-	image = Column(String)
 	url = Column(String)
-	ecommerce = Column(String)
+	image = Column(String)
+	imageUrl = Column(String)
 
-	def __init__(self, name, price, image, url, ecommerce):
+	def __init__(self, sitename, name, price, url, image, image_url ):
+		self.siteName = sitename
 		self.name = name
 		self.price = price
-		self.image = image
 		self.url = url
-		self.ecommerce = ecommerce
+		self.image = image
+		self.imageUrl = image_url
+		
+		
