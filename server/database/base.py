6908733d14ab3abdf4e93e08e7d90ae6bd869d69
refdinal22 <refdinal22@gmail.com>
2020-06-08 12:49:13 +0700

    feat(restAPI-server): add connection to online mysql database

--- server/database/base.py
+++ server/database/base.py
@@ -2,7 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql://root:@localhost:3306/retrieval')
+# remotemysql
+# engine = create_engine('mysql://iXYqDIic4I:FELalLFBt6@remotemysql.com:3306/iXYqDIic4I')
+
+# db4free.net
+engine = create_engine('mysql://tubagus:refdinal123@db4free.net:3306/imagesearch')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
