1c869b38ccc50608fb8d436fa09d4e626339a658
refdinal22 <refdinal22@gmail.com>
2020-08-06 14:35:42 +0700

    feature(restApi-server): change to local database

--- technical_service/database/base.py
+++ technical_service/database/base.py
@@ -6,7 +6,8 @@ from sqlalchemy.orm import sessionmaker
 # engine = create_engine('mysql://iXYqDIic4I:FELalLFBt6@remotemysql.com:3306/iXYqDIic4I')
 
 # db4free.net
-engine = create_engine('mysql://tubagus:refdinal123@db4free.net:3306/imagesearch')
+# engine = create_engine('mysql://tubagus:refdinal123@db4free.net:3306/imagesearch')
+engine = create_engine('mysql://root:@localhost:3306/imagesearch')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
